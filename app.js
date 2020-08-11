279aa0d04d19167c87443fc04815d0656d529c9b
Shota Harada <s_harada@incrementp.co.jp>
2020-08-11 11:29:59 +0900

    feat(app.js): click play button to play sound and video.

---
+++ app.js
@@ -0,0 +1,37 @@
+const app = () => {
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.video-container video');
+
+    // Sounds
+    const sounds = document.querySelectorAll('.sound-picker button');
+    // Time Display
+    const timeDisplay = document.querySelector('.time-display');
+    // Get the length of the outline
+    const outlineLength = outline.getTotalLength();
+    // Duration
+    let fakeDuration = 600;
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+    // play sounds
+    play.addEventListener('click', () => {
+        checkPlaying(song);
+    });
+
+    // Create a function specific to step and play the sounds
+    const checkPlaying = song => {
+        if( song.paused ) {
+            song.play();
+            video.play();
+            play.src = './svg/pause.svg';
+        } else {
+            song.pause();
+            video.pause();
+            play.src = './svg/play.svg';
+        }
+    }
+};
+
+app();
